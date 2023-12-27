1107b94fb47d367f1d9c935421219324bce123a7
Ivan Shokurov <ivanshokuroff@devshokmba.local>
2023-12-27 17:37:12 +0400

    Little fixes

--- MVVM+Builder.xctemplate/___FILEBASENAME___ControllerOutput.swift
+++ MVVM+Builder.xctemplate/___FILEBASENAME___ControllerOutput.swift
@@ -3,6 +3,6 @@ import Foundation
 /// Корневой интерфейс для реализации
 /// вертикальной коммуникации между
 /// **___VARIABLE_productName___ViewController** и **Module**
-/// по схеме: **___VARIABLE_productName___ Controller ---> **Module**.
+/// по схеме: **___VARIABLE_productName___ Controller** ---> **Module**.
 
 protocol ___FILEBASENAMEASIDENTIFIER___: ControllerOutput {}
--- MVVM+Builder.xctemplate/___FILEBASENAME___Model.swift
+++ MVVM+Builder.xctemplate/___FILEBASENAME___Model.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// **Model** для модуля ``Module/ID/___FILEBASENAME___``.
+/// **Model** для модуля ``Module/ID/___VARIABLE_productName___``.
 final class ___FILEBASENAMEASIDENTIFIER___: Model {
     var module: ___VARIABLE_productName___ModelOutput?
 }
